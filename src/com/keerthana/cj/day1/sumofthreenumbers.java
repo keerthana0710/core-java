4a116086021328e2d31a41824265400df42fca1b
Your Name"SIRAMSETTY <you@example.com"keerthanasiramsetty@gail.com>
2021-05-03 05:56:58 +0530

    initial commit

---
+++ src/com/keerthana/cj/day1/sumofthreenumbers.java
@@ -0,0 +1,13 @@
+package com.keerthana.cj.day1;
+
+public class sumofthreenumbers {
+	public static void main(String[] args) {
+
+		int num1 = 30;
+		int num2 = 10;
+		int num3 = 20;
+		int sum = num1+num2+num3;
+		System.out.println("the sum is :"+sum+" ");
+	}
+
+}
---
+++ src/com/keerthana/cj/day1/welcome.java
@@ -0,0 +1,9 @@
+package com.keerthana.cj.day1;
+
+public class welcome {
+	public static void main(String[] args) {
+		String name ="keerthana";
+	System.out.println("hi "+name+" welcome to java world");
+	}
+
+}
